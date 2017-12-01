d978972fbac757b113a7346be8074f8f33839b27
leofdgit <33867485+leofdgit@users.noreply.github.com>
2017-12-01 10:28:39 +0000

    removed print statements

--- player_histories_farm.py
+++ player_histories_farm.py
@@ -2,11 +2,9 @@ import json
 import requests
 
 other_db = requests.get(url='https://fantasy.premierleague.com/drf/bootstrap-static').json()
-print(len(other_db['elements']))
 
 fixtures_infos = {}
 for i in range(1,len(other_db['elements'])+1):
-    print(i)
     other_db1 = requests.get(url='https://fantasy.premierleague.com/drf/element-summary/{}'.format(i)).json()
     fixtures_infos['{}'.format(i)] = other_db1
 
