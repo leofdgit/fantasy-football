602563624554bebdda11b906f1464c8e8f05f5da
leofdgit <33867485+leofdgit@users.noreply.github.com>
2017-12-02 12:25:21 +0000

    Changed file to include both data sources

--- player_histories_farm.py
+++ player_histories_farm.py
@@ -10,3 +10,8 @@ for i in range(1,len(other_db['elements'])+1):
 
 with open("player_fixtures.txt", "w") as g:
     g.write(json.dumps(fixtures_infos))
+
+
+other_db1 = requests.get(url='https://fantasy.premierleague.com/drf/bootstrap-static').json()
+with open("player_infos.txt", "w") as g:
+    g.write(json.dumps(other_db1))
